2f4f10d16622502931f504d1fe8da6d247141616
Michael Fleischer <58041938+M-Fleischer@users.noreply.github.com>
2023-02-16 20:35:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Blueberry Oatmeal')
-streamlit.text('Kale, Spinach&Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range-Egg')
+streamlit.text('🥣Omega 3 Blueberry Oatmeal🥣')
+streamlit.text('🥗Kale, Spinach&Rocket Smoothie🥗')
+streamlit.text('🐔Hard-Boiled Free-Range-Egg🐔')
+streamlit.text('🥑🍞Avocado Toast🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
