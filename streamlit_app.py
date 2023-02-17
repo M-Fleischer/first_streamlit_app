8b9471e965d28b2cc37d50348898c579c83bce86
Michael Fleischer <58041938+M-Fleischer@users.noreply.github.com>
2023-02-17 18:54:28 +0100

    table filter by mulitselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let us add a picklist so that they can select some fruits
-streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc(fruits_selected)
 
 #Show the fruitlist on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
