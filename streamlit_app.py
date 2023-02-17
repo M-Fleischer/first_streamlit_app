1ea12ce1bea911894a166b222ebdd1c395daac49
Michael Fleischer <58041938+M-Fleischer@users.noreply.github.com>
2023-02-17 18:50:03 +0100

    corect typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let us add a picklist so that they can select some fruits
-streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocados', 'Strawberries'])
+streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #Show the fruitlist on the page
 streamlit.dataframe(my_fruit_list)
