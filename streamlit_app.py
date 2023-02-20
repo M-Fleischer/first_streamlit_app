5acab25e8d5c98a35b314c99290f3e1668c2480d
Michael Fleischer <58041938+M-Fleischer@users.noreply.github.com>
2023-02-20 16:31:59 +0100

    Request call added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,10 @@ streamlit.text('🥑🍞Avocado Toast🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
+import requests
+
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -19,3 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Show the fruitlist on the page
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
