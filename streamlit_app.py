1bd65865a758b3b19a5c5680353a1f92b917d87d
Michael Fleischer <58041938+M-Fleischer@users.noreply.github.com>
2023-02-20 16:53:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Show the fruitlist on the page
 streamlit.dataframe(fruits_to_show)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.text(fruityvice_response.json())
